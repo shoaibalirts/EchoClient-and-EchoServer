09767567374367c6fb8ba2cda8eafc0479ce1086
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-09 15:42:07 +0200

    created PersonInfo class that implements Serializable interface

---
+++ PErsonInfo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+import javax.swing.JOptionPane;
+public class PersonInfo implements Serializable {
+    String name;
+    String address;
+    String phoneNum;
+    public void printPersonInfo(String n, String add, String ph){
+        name=n;
+        address=add;
+        phoneNum=ph;
+        JOptionPane.showMessageDialog(null, "Name: "+name+", address: "+address+", PhoneNum: "+phoneNum);
+    }
+}
